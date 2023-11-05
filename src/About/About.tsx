84f5373a71376c64eef87397cedff7c40288bcd3
John <john.muchuku@student.moringaschool.com>
2023-11-05 05:01:36 +0300

    ✅Done

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -3,6 +3,7 @@ import Section_2 from "./Section_2";
 import Section_3 from "./Section_3/Section_3";
 import Section_4 from "./Section_4";
 import Section_5 from "./Section_5";
+import Section_6 from "./Section_6";
 
 
 
@@ -14,6 +15,7 @@ export default function About() {
         <Section_3/>
         <Section_4/>
         <Section_5/>
+        <Section_6/>
         
     </div>
   )
---
+++ src/About/Section_6.tsx
@@ -0,0 +1,34 @@
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+
+export default function Section_6() {
+    const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  // Define the animation variants
+  const animationVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  // Trigger the animation when the element is in the viewport
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    } else {
+      controls.start('hidden');
+    }
+  }, [controls, inView]);
+  return (
+    <div className="container-fluid mt-5 mb-5 d-flex justify-content-center align-item-center" style={{height : "50vh"}}>
+        <motion.h1 
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        variants={animationVariants}
+        transition={{ duration: 1 }}  
+        className="display-4 mt-5 mb-5 text-center text-black ls">Learn more about<br /><a className="fw-bold link-2 link-3-hover link-underline-dark link-underline-opacity-0 link-underline-opacity-75-hover text-black">Elewa's social impact.</a></motion.h1>
+    </div>
+  )
+}
